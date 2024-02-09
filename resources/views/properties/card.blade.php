cd6a82a2b8c92b1191a3782152f7600d2d69ee5a
Nick Tep <nicktep520@gmail.com>
2024-02-09 08:44:41 +0100

    m

--- resources/views/properties/card.blade.php
+++ resources/views/properties/card.blade.php
@@ -3,10 +3,10 @@
         <img src="path/to/image1.jpg" alt="Property Image">
     </div>
     <div class="property-details">
-        <p class="price">Prix: {{ number_format($property->price, thousands_separator: ' ') }} F CFA</p>
-        <p class="location">Ville: {{$property->city?->name_city}}</p>
-        <p class="neighborhood">Quartier: {{$property->neighborhood}} </p>
-        <p class="area">Surface: {{$property->surface}} m²</p>
+        <p class="price">Prix : {{ number_format($property->price, thousands_separator: ' ') }} F CFA</p>
+        <p class="location">Ville : {{$property->city?->name_city}}</p>
+        <p class="neighborhood">Quartier : {{$property->neighborhood}} </p>
+        <p class="area">Surface : {{$property->surface}} m²</p>
     </div>
     <div class="property-actions">
         <!-- La miniature de l'agent immobilier -->
